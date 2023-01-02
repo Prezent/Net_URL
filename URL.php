90912607adb3dd36bcf2f0a3e72b0e7d65e867c0
Alex van Dun <alex@prezent.nl>
2023-01-02 09:57:00 +0000

    Replaced curly brace syntax for leading slash detection

--- URL.php
+++ URL.php
@@ -101,16 +101,6 @@ class Net_URL
     var $useBrackets;
 
     /**
-    * PHP4 Constructor
-    *
-    * @see __construct()
-    */
-    function Net_URL($url = null, $useBrackets = true)
-    {
-        $this->__construct($url, $useBrackets);
-    }
-
-    /**
     * PHP5 Constructor
     *
     * Parses the given url and stores the various parts
@@ -190,7 +180,7 @@ class Net_URL
                         break;
 
                     case 'path':
-                        if ($value{0} == '/') {
+                        if (substr($value, 0, 1) === '/') {
                             $this->path = $value;
                         } else {
                             $path = dirname($this->path) == DIRECTORY_SEPARATOR ? '' : dirname($this->path);
